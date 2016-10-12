270d20c7e66963d01e03169a3a3a6720c314ecbc
Juan Pablo Ramírez <juan.ramirez.q@gmail.com>
2016-10-12 15:35:26 -0500

    added hazium data

---
+++ src/components/Hazium.js
@@ -0,0 +1,27 @@
+import React from 'react';
+
+module.exports = () => {
+  return (
+    <div className="row">
+      <div className="col-md-12">
+        <div className="page-header">
+          <h1>
+            Hazium sensors data
+          </h1>
+        </div>
+      </div>
+      <div className="col-md-12">
+        <div className="panel panel-default">
+          <div className="panel-body">
+            aqui irá el gráfico
+          </div>
+        </div>
+      </div>
+      <div className="col-md-12">
+        <div className="well">
+          Aqui va a ir la descripción del gráfico.
+        </div>
+      </div>
+    </div>
+  );
+}
--- src/components/Root.js
+++ src/components/Root.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { Router, Route, browserHistory, IndexRoute } from 'react-router';
 import Template from './Template';
 import Home from './Home';
+import Hazium from './Hazium';
 import Test from './Test';
 
 module.exports = () => {
@@ -10,6 +11,7 @@ module.exports = () => {
       <Route path="/" component={Template}>
         <IndexRoute component={Home} />
         <Route path="/" component={Home} />
+        <Route path="/hazium" component={Hazium} />
         <Route path="/test" component={Test} />
       </Route>
     </Router>
